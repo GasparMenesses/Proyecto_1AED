9b596856459b18983c50b2041cdbb15301226c7a
JoseCarlos2605 <josecarlos.gf26@gmail.com>
2026-04-27 16:26:18 -0300

    Clase experimento
    se creo la clase experimento, con sus atributos

---
+++ src/main/java/org/example/tda/Experimento.java
@@ -0,0 +1,9 @@
+package org.example.tda;
+public class Experimento {
+    private String id;
+    private DataSet dataset;    
+    private Modelo modelo;
+    private String estado; //pendiente o ejecutado
+    double exactitud = 0; 
+    double precision = 0; 
+}
